c90a0315685b2e2a07887798de9d15ea36721ee2
matsurai <m.matsurai25@gmail.com>
2020-01-02 18:09:06 +0900

    いくつかのデフォルトアニメーションを追加

--- src/Intersection.tsx
+++ src/Intersection.tsx
@@ -1,21 +1,43 @@
 import * as React from 'react'
 import 'intersection-observer'
+import styled, { css } from 'styled-components'
 
-type Props = {
-  children: React.ReactNode
-}
-
-export enum INTERSECT_CLASSNAME {
+export enum IntersectClassName {
   BEFORE = 'intersect',
   AFTER = 'intersected'
+}
+
+export enum IntersectAnimation {
+  FadeIn = 'fadeIn',
+  SlideInFromRight = 'slideInFromRight',
+  SlideInFromLeft = 'slideInFromLeft',
+  SlideInFromBottom = 'slideInFromBottom'
+}
+
+interface IntersectFunction {
+  (isIntersected: boolean): React.ReactNode
+}
+
+interface AnimationOption {
+  type: IntersectAnimation
+}
+
+type Props = {
+  children: React.ReactNode | IntersectFunction
+  animationOption?: AnimationOption
 }
 
 /**
  * Intersection ObserverのWrapper
  *
- * このdivに.intersectedが付くので子要素でスタイルを充てる
+ * A. このdivに.intersectedが付くので子要素でスタイルを充てる
+ * B. 子要素に関数を渡してisIntersectedを受け取る
+ * C. アニメーションを指定する
  */
-export default function Intersection({ children }: Props) {
+export default function Intersection({
+  children,
+  animationOption
+}: Props) {
   const [isIntersected, setIntersected] = React.useState(
     false
   )
@@ -44,15 +66,85 @@ export default function Intersection({ children }: Props) {
   }, [callback])
 
   return (
-    <div
+    <Wrapper
+      opt={animationOption}
+      isIntersected={isIntersected}
       ref={ref}
       className={
         isIntersected
-          ? INTERSECT_CLASSNAME.AFTER
-          : INTERSECT_CLASSNAME.BEFORE
+          ? IntersectClassName.AFTER
+          : IntersectClassName.BEFORE
       }
     >
-      {children}
-    </div>
+      {typeof children == 'function'
+        ? children(isIntersected)
+        : children}
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div<{
+  opt?: AnimationOption
+  isIntersected: boolean
+}>`
+  transition: all 0.5s ease;
+
+  ${({ opt }) => {
+    if (!opt) {
+      return null
+    }
+
+    switch (opt.type) {
+      case IntersectAnimation.FadeIn:
+        return css<{ isIntersected: boolean }>`
+          opacity: 0;
+          will-change: opacity;
+
+          ${({ isIntersected }) =>
+            isIntersected &&
+            css`
+              opacity: 1;
+            `}
+        `
+
+      case IntersectAnimation.SlideInFromRight:
+        return css<{ isIntersected: boolean }>`
+          opacity: 0;
+          will-change: opacity, transform;
+          transform: translate3D(200px, 0, 0);
+          ${({ isIntersected }) =>
+            isIntersected &&
+            css`
+              opacity: 1;
+              transform: translate3D(0, 0, 0);
+            `};
+        `
+      case IntersectAnimation.SlideInFromLeft:
+        return css<{ isIntersected: boolean }>`
+          opacity: 0;
+          will-change: opacity, transform;
+          transform: translate3D(-200px, 0, 0);
+          ${({ isIntersected }) =>
+            isIntersected &&
+            css`
+              opacity: 1;
+              transform: translate3D(0, 0, 0);
+            `};
+        `
+      case IntersectAnimation.SlideInFromBottom:
+        return css<{ isIntersected: boolean }>`
+          opacity: 0;
+          will-change: opacity, transform;
+          transform: translate3D(0, 200px, 0);
+          ${({ isIntersected }) =>
+            isIntersected &&
+            css`
+              opacity: 1;
+              transform: translate3D(0, 0, 0);
+            `};
+        `
+      default:
+        return null
+    }
+  }}
+`
--- src/Sample.tsx
+++ src/Sample.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
-import styled from 'styled-components'
-import Intersection from './Intersection'
+import styled, { css } from 'styled-components'
+import Intersection, {
+  IntersectAnimation
+} from './Intersection'
 
 export default function() {
   return (
@@ -12,6 +14,34 @@ export default function() {
             <Item i={i}>Item {i}</Item>
           </Intersection>
         ))}
+      <Intersection key={'q'}>
+        {isIntersected => (
+          <ItemWithFunc isIntersected={isIntersected}>
+            ItemWithFunc
+          </ItemWithFunc>
+        )}
+      </Intersection>
+      <Intersection
+        animationOption={{
+          type: IntersectAnimation.FadeIn
+        }}
+      >
+        <ItemStatic>FadeIn</ItemStatic>
+      </Intersection>
+      <Intersection
+        animationOption={{
+          type: IntersectAnimation.SlideInFromRight
+        }}
+      >
+        <ItemStatic>SlideInFromRight</ItemStatic>
+      </Intersection>
+      <Intersection
+        animationOption={{
+          type: IntersectAnimation.SlideInFromBottom
+        }}
+      >
+        <ItemStatic>SlideInFromBottom</ItemStatic>
+      </Intersection>
     </Wrapper>
   )
 }
@@ -27,8 +57,8 @@ const Item = styled.div<{ i: number }>`
   display: grid;
   justify-items: center;
   align-items: center;
-  background: #faa;
-  filter: hue-rotate(${({ i }) => i * 10}deg);
+  background: linear-gradient(-45deg, #0000ff, #ff9cff);
+  filter: hue-rotate(${({ i }) => i * 30}deg);
   transition: transform 0.5s cubic-bezier(0, 0.8, 0.2, 1),
     opacity 0.5s ease;
   will-change: transform, opacity;
@@ -42,4 +72,36 @@ const Item = styled.div<{ i: number }>`
     opacity: 1;
     transform: translate3d(0, 0, 0);
   }
+`
+
+const ItemWithFunc = styled.div<{ isIntersected: boolean }>`
+  font-size: 100px;
+  height: 300px;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  background: #faa;
+  transition: transform 0.5s cubic-bezier(0, 0.8, 0.2, 1),
+    opacity 0.5s ease;
+  will-change: transform, opacity;
+
+  ${({ isIntersected }) =>
+    isIntersected
+      ? css`
+          opacity: 1;
+          transform: translate3d(0, 0, 0);
+        `
+      : css`
+          opacity: 0;
+          transform: translate3d(300px, 0, 0);
+        `}
+`
+
+const ItemStatic = styled.div`
+  font-size: 100px;
+  height: 300px;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  background: #faa;
 `
